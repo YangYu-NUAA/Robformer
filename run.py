878063c9d0adec0b2e625a1a5fb0bb1689a36aeb
Yang Yu <35400026+YangYu-NUAA@users.noreply.github.com>
2022-10-21 22:38:23 +0800

    Update run.py

--- run.py
+++ run.py
@@ -11,9 +11,6 @@ torch.manual_seed(fix_seed)
 np.random.seed(fix_seed)
 #initial
 parser = argparse.ArgumentParser(description='Autoformer & Transformer family for Time Series Forecasting')
-#--   biaoshi kexuan canshu
-#
-
 
 # basic config
 parser.add_argument('--is_training', type=int, required=True, default=1, help='status')
@@ -34,20 +31,20 @@ parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='l
 
 # forecasting task
 parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
-parser.add_argument('--label_len', type=int, default=48, help='start token length')  #yuanlai 48
+parser.add_argument('--label_len', type=int, default=48, help='start token length') 
 parser.add_argument('--pred_len', type=int, default=192, help='prediction sequence length')
 
 # model define
-parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')  ###duoge yuce dangranshi quanbu tezheng weidu 7
+parser.add_argument('--enc_in', type=int, default=7, help='encoder input size') 
 parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
-parser.add_argument('--c_out', type=int, default=7, help='output size')   #yuanlai shi 7
+parser.add_argument('--c_out', type=int, default=7, help='output size')   
 parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
 parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
 parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
 parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
 parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
 parser.add_argument('--moving_avg', type=int, default=25, help='window size of moving average')
-parser.add_argument('--factor', type=int, default=1, help='attn factor')  #yuanlai shi1
+parser.add_argument('--factor', type=int, default=1, help='attn factor') 
 parser.add_argument('--distil', action='store_false',
                     help='whether to use distilling in encoder, using this argument means not using distilling',
                     default=True)
@@ -64,7 +61,7 @@ parser.add_argument('--itr', type=int, default=1, help='experiments times')  #yu
 parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
 parser.add_argument('--batch_size', type=int, default=64, help='batch size of train input data')
 parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
-parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')  #0.0001
+parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')  
 parser.add_argument('--des', type=str, default='test', help='exp description')
 parser.add_argument('--loss', type=str, default='mse', help='loss function')
 parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
@@ -77,7 +74,7 @@ parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple g
 parser.add_argument('--devices', type=str, default=' 0,1,2, 3', help='device ids of multile gpus')# 0,1,2,3
 
 args = parser.parse_args()
-##############important
+
 
 
 args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
